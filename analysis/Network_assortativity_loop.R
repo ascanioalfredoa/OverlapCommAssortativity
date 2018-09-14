6f1ca7e64353a4093f2b5a14f877d25914cdd3cf
Alfredo Ascanio <ascanio.alfredoa@gmail.com>
2018-09-14 15:32:05 -0400

    Modified loop

--- analysis/Network_assortativity_loop.R
+++ analysis/Network_assortativity_loop.R
@@ -5,14 +5,13 @@ names(loaded_networks) <- c("Graph", "RawData")
 
 #### Results matrix ####
 
-Results <- data.frame(array(data = 0, dim = c(nrow(loaded_networks), 3), dimnames = list(NULL, c("Data", "Shizuka", "Ascanio"))))
+Results <- data.frame(array(data = 0, dim = c(nrow(loaded_networks), 4), dimnames = list(NULL, c("Data", "Shizuka_boot", "Ascanio_boot", "Ascanio_perm"))))
 
 i <- 1
 
 Results$Data[i] <- gsub("_g", "", loaded_networks$Graph[i])
-Results$Shizuka
-
-calc_rc(data = eval(parse(text = loaded_networks$RawData[i])), 
-        n.bootstraps = 100, gbi.format = F)
+Results$Shizuka_boot[i] <- calc_rc(data = eval(parse(text = loaded_networks$RawData[i])), 
+                           n.bootstraps = 100, gbi.format = F)
 
 
+Results
